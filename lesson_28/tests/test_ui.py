bb49f200229ab8abc824e246e612810532a3f504
Volodymyr Dzhevalov <vladtestac@gmail.com>
2025-08-21 21:22:25 +0300

    Переробив під фікстури як було в матеріалах лекції

--- lesson_28/tests/test_ui.py
+++ lesson_28/tests/test_ui.py
@@ -1,26 +1,10 @@
 import pytest
-from lesson_28.forms.login_form import LoginForm
-from lesson_28.pages.garage_page import GaragePage
-from lesson_28.pages.home_page import HomePage
-import time
 
-@pytest.mark.parametrize("login, password", [
-    ("123_aa@test.com", "12345678aA")])
-def test_successful_login_form(driver, login, password):
-    home_page = HomePage(driver).open()
-    home_page.click_sign_in_button()
-
-    login_form = LoginForm(driver)
-    login_form.get_password_input_field().send_keys(password)
-    login_form.get_email_input_field().send_keys(login)
-    login_form.get_login_button().click()
-
-    garage_page = GaragePage(driver)
-    garage_page.wait_until_expenses_present(timeout_seconds=5)
-
-    fuel_expenses = garage_page.get_fuel_expenses_button()
-    instructions = garage_page.get_instructions_button()
-    garage = garage_page.get_garage_button()
+def test_successful_login_form(driver, open_home_page, open_sign_in_page, sign_in, check_successful_login):
+    open_home_page()
+    open_sign_in_page()
+    sign_in()
+    fuel_expenses, instructions, garage = check_successful_login()
 
     assert fuel_expenses.is_displayed()
     assert instructions.is_displayed()
